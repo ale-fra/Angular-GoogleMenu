e8e9105734ac22a6c922b579c52fb4ec0524dacd
justBorn <alessandro.francia@gmail.com>
2014-03-11 19:41:44 +0100

    test commit

--- GoogleNexusMenu/Demo/app.js
+++ GoogleNexusMenu/Demo/app.js
@@ -1,87 +1,101 @@
-var App = angular.module('App', []);
+var App = angular.module('App', [])
 
-function topMenuController($scope, $window) {
-	$scope.$window = $window;
-	$scope.showAll = false;
-	$scope.showPart = false;
-	$scope.href = "http://tympanus.net/Tutorials/GoogleNexusWebsiteMenu/"
-		$scope.text ="This is a simple porting of GoogleN menu "
-	$scope.itemsMenu = [ {
-		name : 'Home',
-		href : '#home',
-		icon : 'my-icon-home'
-	}, { 
-		name : 'Setting',
-		href : '#setting',
-		icon : 'my-icon-archive'
-	}, {
-		name : 'Portfolio',
-		href : '#portfolio',
-		icon : 'my-icon-pictures'
-	}, {
-		name : 'Blog',
-		href : '#blog',
-		icon : 'my-icon-article'
-	}, {
-		name : 'Prices',
-		href : '#prices',
-		icon : 'my-icon-bars'
-	} ];
+.service('MenuService', [ '$rootScope', function($rootScope) {
+	return {
+		itemsMenu : [ {
+			name : 'Home',
+			href : '#home',
+			icon : 'my-icon-home'
+		}, {
+			name : 'Setting',
+			href : '#setting',
+			icon : 'my-icon-archive'
+		}, {
+			name : 'Portfolio',
+			href : '#portfolio',
+			icon : 'my-icon-pictures'
+		}, {
+			name : 'Blog',
+			href : '#blog',
+			icon : 'my-icon-article'
+		}, {
+			name : 'Prices',
+			href : '#prices',
+			icon : 'my-icon-bars'
+		} ],
 
-	$scope.mouseenter = function() {
-		if (!$scope.showAll)
-			$scope.showPart = true;
+		add : function(item) {
+			this.menu.push(item);
+			$rootScope.$broadcast('MenuService.update', this.menu);
+		},
+		open : {"part" : false ,	"all" : false },
+
+		setShowPart : function(val) {
+			this.open.part = val;
+			$rootScope.$broadcast('MenuService.update', this.open);
+
+		},
+		setShowAll : function(val) {
+			this.open.all = val;
+			if (val)
+				this.open.part = false;
+			$rootScope.$broadcast('MenuService.update', this.open);
+
+		}
 	};
+} ])
 
-	$scope.mouseleave = function() {
-		if (!$scope.showAll)
-			$scope.showPart = false;
+.controller('topMenuController',
+		[ 'MenuService', '$scope', function(MenuService, $scope) {
+			$scope.itemsMenu = MenuService.itemsMenu;
+			$scope.showAll = MenuService.open.all;
+			$scope.showPart = MenuService.open.part;
+
+			$scope.mouseenter = function() {
+				if (!$scope.showAll)
+					MenuService.setShowPart(true);
+			};
+
+			$scope.mouseleave = function() {
+				if (!$scope.showAll)
+					MenuService.setShowPart(false);
+			};
+
+			$scope.menuHover = function(event) {
+				if ($scope.showPart == true)
+					$scope.menuTogle(event);
+			}
+
+			
+			$scope.menuTogle = function(event) {
+				event.stopPropagation();
+				MenuService.setShowAll(true);
+			};
+
+			function closeMenu() {
+				$scope.showAll = false;
+				$scope.$window.onclick = null;
+
+			}
+			
+			$scope.$on('MenuService.update', function(event, open) {
+				$scope.showAll = open.all;
+				$scope.showPart = open.part;
+			});
+
+		} ])
+		
+.controller('contentBodyController',
+[ 'MenuService', '$scope', function(MenuService, $scope) {
+	$scope.isMenuClosed = !MenuService.open.all;
+
+	$scope.closeMenu = function() {
+			MenuService.setShowAll(false);
 	};
 	
-	$scope.menuHover = function(event){
-		if($scope.showPart == true)
-			$scope.menuTogle(event);
-	}
-
-	$scope.menuTogle = function(event) {
-			event.stopPropagation();
-		$scope.showAll = !$scope.showAll;
-		$scope.showPart = false;
-
-		if ($scope.showAll) {
-			$scope.$window.onclick = function(event) {
-				closeSearchWhenClickingElsewhere(event, $scope.menuTogle);
-				$scope.$apply();
-			};
-		} else
-			closeMenu();
-	};
+	$scope.$on('MenuService.update', function(event, open) {
+		$scope.isMenuClosed = !open.all;
+	});
 
 
-	function closeMenu() {
-		$scope.showAll = false;
-		$scope.$window.onclick = null;
-
-	}
-
-	function closeSearchWhenClickingElsewhere(event, callbackOnClose) {
-
-		var clickedElement = event.target;
-		if (!clickedElement)
-			return;
-		var elementParent = clickedElement.parentElement;
-		if (!elementParent)
-			return;
-		var elementParentClasses = clickedElement.parentElement.classList;
-		var clickedOnSearchDrawer = elementParentClasses.contains('menu-item')
-				|| elementParentClasses.contains('gn-menu');
-		console.log("sei nel menu : " + clickedOnSearchDrawer);
-
-		if (!clickedOnSearchDrawer) {
-			closeMenu()
-			return;
-		}
-
-	}
-
-}
+} ]);
